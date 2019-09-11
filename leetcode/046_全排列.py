e4f3b1c349220cd0e33101e25f6d182a6f255ef8
sethGu <602841235@gmail.com>
2019-09-11 21:06:39 +1000

    2019.9月11日

---
+++ leetcode/046_全排列.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(nums, tmp):
+            if not nums:
+                res.append(tmp)
+                return
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i + 1:], tmp + [nums[i]])
+
+        backtrack(nums, [])
+        return res
+
+
+a=Solution()
+print(a.permute([1,2,3]))
